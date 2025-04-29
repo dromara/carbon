6a77e5063b51ce0dcfc9f56a8a9849f20e817356
kuafuRace <245629560@qq.com>
2025-04-29 11:57:40 +0800

    refactor: Replace string concatenation with `filepath.Join()`

--- language.go
+++ language.go
@@ -3,6 +3,7 @@ package carbon
 import (
 	"embed"
 	"encoding/json"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
@@ -25,7 +26,7 @@ type Language struct {
 // 初始化 Language 结构体
 func NewLanguage() *Language {
 	return &Language{
-		dir:       "lang/",
+		dir:       "lang",
 		locale:    DefaultLocale,
 		resources: make(map[string]string),
 		rw:        new(sync.RWMutex),
@@ -48,7 +49,7 @@ func (lang *Language) SetLocale(locale string) *Language {
 	defer lang.rw.Unlock()
 
 	lang.locale = locale
-	fileName := lang.dir + locale + ".json"
+	fileName := filepath.Join(lang.dir, locale+".json")
 	bytes, err := fs.ReadFile(fileName)
 	if err != nil {
 		lang.Error = ErrNotExistLocale(fileName)
