d08abed828baa90e3d8b92084567c3842cb1f95d
kuafuRace <245629560@qq.com>
2025-05-26 10:29:32 +0800

    feat: Add support multiple parameters for Closest and Farthest methods

--- extremum.go
+++ extremum.go
@@ -44,10 +44,15 @@ func MinDuration() Duration {
 // Max returns the maximum Carbon instance from the given Carbon instance (second-precision).
 func Max(c1 Carbon, c2 ...Carbon) (c Carbon) {
 	c = c1
+	if c.IsInvalid() {
+		return
+	}
+	if len(c2) == 0 {
+		return
+	}
 	for i := range c2 {
-		if c.IsInvalid() || c2[i].IsInvalid() {
-			c.isEmpty = true
-			return c
+		if c2[i].IsInvalid() {
+			return c2[i]
 		}
 		if c2[i].Gte(c) {
 			c = c2[i]
@@ -59,10 +64,15 @@ func Max(c1 Carbon, c2 ...Carbon) (c Carbon) {
 // Min returns the minimum Carbon instance from the given Carbon instance (second-precision).
 func Min(c1 Carbon, c2 ...Carbon) (c Carbon) {
 	c = c1
+	if c.IsInvalid() {
+		return
+	}
+	if len(c2) == 0 {
+		return
+	}
 	for i := range c2 {
-		if c.IsInvalid() || c2[i].IsInvalid() {
-			c.isEmpty = true
-			return c
+		if c2[i].IsInvalid() {
+			return c2[i]
 		}
 		if c2[i].Lte(c) {
 			c = c2[i]
@@ -71,26 +81,62 @@ func Min(c1 Carbon, c2 ...Carbon) (c Carbon) {
 	return
 }
 
-// Closest returns the closest Carbon instance from the given Carbon instance.
-func (c Carbon) Closest(c1 Carbon, c2 Carbon) Carbon {
-	if c.IsInvalid() || c1.IsInvalid() || c2.IsInvalid() {
-		c.isEmpty = true
+// Closest returns the closest Carbon instance from some given Carbon instances.
+func (c Carbon) Closest(c1 Carbon, c2 ...Carbon) Carbon {
+	if c.IsInvalid() {
 		return c
 	}
-	if c.DiffAbsInSeconds(c1) < c.DiffAbsInSeconds(c2) {
+	if c1.IsInvalid() {
 		return c1
 	}
-	return c2
+	if len(c2) == 0 {
+		return c1
+	}
+	args := append([]Carbon{c1}, c2...)
+	for i := range args {
+		if args[i].IsInvalid() {
+			return args[i]
+		}
+	}
+	closest := args[0]
+	minDiff := c.DiffAbsInSeconds(closest)
+	for i := range args[1:] {
+		arg := args[1:][i]
+		diff := c.DiffAbsInSeconds(arg)
+		if diff < minDiff {
+			minDiff = diff
+			closest = arg
+		}
+	}
+	return closest
 }
 
-// Farthest returns the farthest Carbon instance from the given Carbon instance.
-func (c Carbon) Farthest(c1 Carbon, c2 Carbon) Carbon {
-	if c.IsInvalid() || c1.IsInvalid() || c2.IsInvalid() {
-		c.isEmpty = true
+// Farthest returns the farthest Carbon instance from some given Carbon instances.
+func (c Carbon) Farthest(c1 Carbon, c2 ...Carbon) Carbon {
+	if c.IsInvalid() {
 		return c
 	}
-	if c.DiffAbsInSeconds(c1) > c.DiffAbsInSeconds(c2) {
+	if c1.IsInvalid() {
 		return c1
 	}
-	return c2
+	if len(c2) == 0 {
+		return c1
+	}
+	args := append([]Carbon{c1}, c2...)
+	for i := range args {
+		if args[i].IsInvalid() {
+			return args[i]
+		}
+	}
+	farthest := args[0]
+	maxDiff := c.DiffAbsInSeconds(farthest)
+	for i := range args[1:] {
+		arg := args[1:][i]
+		diff := c.DiffAbsInSeconds(arg)
+		if diff > maxDiff {
+			maxDiff = diff
+			farthest = arg
+		}
+	}
+	return farthest
 }
--- extremum_test.go
+++ extremum_test.go
@@ -31,73 +31,157 @@ func TestMinDuration(t *testing.T) {
 }
 
 func TestMax(t *testing.T) {
-	t.Run("zero time", func(t *testing.T) {
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Max(NewCarbon(), NewCarbon()).ToString())
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Max(NewCarbon(), Parse("2021-08-05")).ToString())
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Max(Parse("2021-08-05"), NewCarbon()).ToString())
+	c1 := Parse("2020-08-01")
+	c2 := Parse("2020-08-03")
+	c3 := Parse("2020-08-06")
+
+	t.Run("zero carbon", func(t *testing.T) {
+		c := NewCarbon()
+		assert.Equal(t, c3, Max(c, c1, c2, c3))
+		assert.Equal(t, c3, Max(c1, c, c2, c3))
+		assert.Equal(t, c3, Max(c2, c1, c, c3))
+		assert.Equal(t, c3, Max(c3, c1, c2, c))
 	})
 
-	t.Run("invalid time", func(t *testing.T) {
-		assert.Empty(t, Max(Parse(""), Tomorrow()).ToString())
-		assert.Empty(t, Max(Parse("0"), Tomorrow()).ToString())
-		assert.Empty(t, Max(Now(), Parse("xxx")).ToString())
+	t.Run("empty carbon", func(t *testing.T) {
+		c := Parse("")
+		assert.Empty(t, Max(c, c1, c2, c3).ToString())
+		assert.Empty(t, Max(c1, c, c2, c3).ToString())
+		assert.Empty(t, Max(c2, c1, c, c3).ToString())
+		assert.Empty(t, Max(c3, c1, c2, c).ToString())
 	})
 
-	t.Run("valid time", func(t *testing.T) {
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Max(Parse("2020-08-06"), Parse("2021-08-05")).ToString())
+	t.Run("error carbon", func(t *testing.T) {
+		c := Parse("xxx")
+		assert.Error(t, Max(c, c1, c2, c3).Error)
+		assert.Error(t, Max(c1, c, c2, c3).Error)
+		assert.Error(t, Max(c2, c1, c, c3).Error)
+		assert.Error(t, Max(c3, c1, c2, c).Error)
+	})
+
+	t.Run("valid carbon", func(t *testing.T) {
+		assert.Equal(t, c1, Max(c1))
+		assert.Equal(t, c2, Max(c1, c2))
+		assert.Equal(t, c3, Max(c1, c2, c3))
 	})
 }
 
 func TestMin(t *testing.T) {
-	t.Run("zero time", func(t *testing.T) {
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Min(NewCarbon(), NewCarbon()).ToString())
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Min(NewCarbon(), Parse("2021-08-05")).ToString())
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Min(Parse("2021-08-05"), NewCarbon()).ToString())
+	c1 := Parse("2020-08-01")
+	c2 := Parse("2020-08-03")
+	c3 := Parse("2020-08-06")
+
+	t.Run("zero carbon", func(t *testing.T) {
+		c := NewCarbon()
+		assert.Equal(t, c, Min(c, c1, c2, c3))
+		assert.Equal(t, c, Min(c1, c, c2, c3))
+		assert.Equal(t, c, Min(c2, c1, c, c3))
+		assert.Equal(t, c, Min(c3, c1, c2, c))
 	})
 
-	t.Run("invalid time", func(t *testing.T) {
-		assert.Empty(t, Min(Parse(""), Tomorrow()).ToString())
-		assert.Empty(t, Min(Parse("0"), Tomorrow()).ToString())
-		assert.Empty(t, Min(Now(), Parse("xxx")).ToString())
+	t.Run("empty carbon", func(t *testing.T) {
+		c := Parse("")
+		assert.Empty(t, Min(c, c1, c2, c3).ToString())
+		assert.Empty(t, Min(c1, c, c2, c3).ToString())
+		assert.Empty(t, Min(c2, c1, c, c3).ToString())
+		assert.Empty(t, Min(c3, c1, c2, c).ToString())
 	})
 
-	t.Run("valid time", func(t *testing.T) {
-		assert.Equal(t, "2020-08-06 00:00:00 +0000 UTC", Min(Parse("2020-08-06"), Parse("2021-08-05")).ToString())
+	t.Run("error carbon", func(t *testing.T) {
+		c := Parse("xxx")
+		assert.Error(t, Min(c, c1, c2, c3).Error)
+		assert.Error(t, Min(c1, c, c2, c3).Error)
+		assert.Error(t, Min(c2, c1, c, c3).Error)
+		assert.Error(t, Min(c3, c1, c2, c).Error)
+	})
+
+	t.Run("valid carbon", func(t *testing.T) {
+		assert.Equal(t, c1, Min(c1))
+		assert.Equal(t, c1, Min(c1, c2))
+		assert.Equal(t, c1, Min(c1, c2, c3))
 	})
 }
 
 func TestCarbon_Closest(t *testing.T) {
-	t.Run("zero time", func(t *testing.T) {
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", NewCarbon().Closest(NewCarbon(), NewCarbon()).ToString())
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Parse("2020-08-05").Closest(NewCarbon(), Parse("2021-08-05")).ToString())
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Parse("2020-08-05").Closest(Parse("2021-08-05"), NewCarbon()).ToString())
+	c1 := Parse("2020-08-01")
+	c2 := Parse("2020-08-03")
+	c3 := Parse("2020-08-06")
+
+	t.Run("zero carbon", func(t *testing.T) {
+		c := NewCarbon()
+		assert.Equal(t, c1, c.Closest(c1, c2, c3))
+		assert.Equal(t, c2, c1.Closest(c, c2, c3))
+		assert.Equal(t, c1, c2.Closest(c1, c, c3))
+		assert.Equal(t, c2, c3.Closest(c1, c2, c))
 	})
 
-	t.Run("invalid time", func(t *testing.T) {
-		assert.Empty(t, Parse("").Closest(Now(), Tomorrow()).ToString())
-		assert.Empty(t, Parse("0").Closest(Now(), Tomorrow()).ToString())
-		assert.Empty(t, Parse("xxx").Closest(Now(), Tomorrow()).ToString())
+	t.Run("empty carbon", func(t *testing.T) {
+		c := Parse("")
+		assert.Equal(t, c, c.Closest(c1, c2, c3))
+		assert.Equal(t, c, c1.Closest(c, c2, c3))
+		assert.Equal(t, c, c2.Closest(c1, c, c3))
+		assert.Equal(t, c, c3.Closest(c1, c2, c))
 	})
 
-	t.Run("valid time", func(t *testing.T) {
-		assert.Equal(t, "2020-08-06 00:00:00 +0000 UTC", Parse("2020-08-05").Closest(Parse("2020-08-06"), Parse("2021-08-05")).ToString())
+	t.Run("error carbon", func(t *testing.T) {
+		c := Parse("xxx")
+		assert.Equal(t, c, c.Closest(c1, c2, c3))
+		assert.Equal(t, c, c1.Closest(c, c2, c3))
+		assert.Equal(t, c, c2.Closest(c1, c, c3))
+		assert.Equal(t, c, c3.Closest(c1, c2, c))
+	})
+
+	t.Run("valid carbon", func(t *testing.T) {
+		c := Parse("2020-08-05")
+		assert.Equal(t, c, c.Closest(c))
+		assert.Equal(t, c1, c.Closest(c1))
+		assert.Equal(t, c2, c.Closest(c1, c2))
+		assert.Equal(t, c3, c.Closest(c1, c3))
+		assert.Equal(t, c3, c.Closest(c1, c2, c3))
+		assert.Equal(t, c2, c1.Closest(c, c2, c3))
+		assert.Equal(t, c1, c2.Closest(c1, c, c3))
+		assert.Equal(t, c, c3.Closest(c1, c2, c))
 	})
 }
 
 func TestCarbon_Farthest(t *testing.T) {
-	t.Run("zero time", func(t *testing.T) {
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", NewCarbon().Farthest(NewCarbon(), NewCarbon()).ToString())
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Parse("2020-08-05").Farthest(NewCarbon(), Parse("2021-08-05")).ToString())
-		assert.Equal(t, "0001-01-01 00:00:00 +0000 UTC", Parse("2020-08-05").Farthest(Parse("2021-08-05"), NewCarbon()).ToString())
+	c1 := Parse("2020-08-01")
+	c2 := Parse("2020-08-03")
+	c3 := Parse("2020-08-06")
+
+	t.Run("zero carbon", func(t *testing.T) {
+		c := NewCarbon()
+		assert.Equal(t, c3, c.Farthest(c1, c2, c3))
+		assert.Equal(t, c, c1.Farthest(c, c2, c3))
+		assert.Equal(t, c, c2.Farthest(c1, c, c3))
+		assert.Equal(t, c, c3.Farthest(c1, c2, c))
 	})
 
-	t.Run("invalid time", func(t *testing.T) {
-		assert.Empty(t, Parse("").Farthest(Now(), Tomorrow()).ToString())
-		assert.Empty(t, Parse("0").Farthest(Now(), Tomorrow()).ToString())
-		assert.Empty(t, Parse("xxx").Farthest(Now(), Tomorrow()).ToString())
+	t.Run("empty carbon", func(t *testing.T) {
+		c := Parse("")
+		assert.Equal(t, c, c.Farthest(c1, c2, c3))
+		assert.Equal(t, c, c1.Farthest(c, c2, c3))
+		assert.Equal(t, c, c2.Farthest(c1, c, c3))
+		assert.Equal(t, c, c3.Farthest(c1, c2, c))
 	})
 
-	t.Run("valid time", func(t *testing.T) {
-		assert.Equal(t, "2021-08-05 00:00:00 +0000 UTC", Parse("2020-08-05").Farthest(Parse("2020-08-06"), Parse("2021-08-05")).ToString())
+	t.Run("error carbon", func(t *testing.T) {
+		c := Parse("xxx")
+		assert.Equal(t, c, c.Farthest(c1, c2, c3))
+		assert.Equal(t, c, c1.Farthest(c, c2, c3))
+		assert.Equal(t, c, c2.Farthest(c1, c, c3))
+		assert.Equal(t, c, c3.Farthest(c1, c2, c))
+	})
+
+	t.Run("valid carbon", func(t *testing.T) {
+		c := Parse("2020-08-05")
+		assert.Equal(t, c, c.Farthest(c))
+		assert.Equal(t, c1, c.Farthest(c1))
+		assert.Equal(t, c1, c.Farthest(c1, c2))
+		assert.Equal(t, c1, c.Farthest(c1, c3))
+		assert.Equal(t, c1, c.Farthest(c1, c2, c3))
+		assert.Equal(t, c3, c1.Farthest(c, c2, c3))
+		assert.Equal(t, c3, c2.Farthest(c1, c, c3))
+		assert.Equal(t, c1, c3.Farthest(c1, c2, c))
 	})
 }
