1607850a8217279faf3e896ee182280a5437a13a
kuafuRace <245629560@qq.com>
2025-06-10 19:30:47 +0800

    docs: Update Japanese search text label

--- .vitepress/config/locales/jp.ts
+++ .vitepress/config/locales/jp.ts
@@ -131,42 +131,42 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
 
 export const search: DefaultTheme.AlgoliaSearchOptions['locales'] = {
     jp: {
-        placeholder: '搜索文档',
+        placeholder: 'ドキュメントの検索',
         translations: {
             button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
+                buttonText: 'ドキュメントの検索',
+                buttonAriaLabel: 'ドキュメントの検索'
             },
             modal: {
                 searchBox: {
-                    resetButtonTitle: '清除查询条件',
-                    resetButtonAriaLabel: '清除查询条件',
-                    cancelButtonText: '取消',
-                    cancelButtonAriaLabel: '取消'
+                    resetButtonTitle: 'クエリー条件の消去',
+                    resetButtonAriaLabel: 'クエリー条件の消去',
+                    cancelButtonText: 'キャンセル',
+                    cancelButtonAriaLabel: 'キャンセル'
                 },
                 startScreen: {
-                    recentSearchesTitle: '搜索历史',
-                    noRecentSearchesText: '没有搜索历史',
-                    saveRecentSearchButtonTitle: '保存至搜索历史',
-                    removeRecentSearchButtonTitle: '从搜索历史中移除',
-                    favoriteSearchesTitle: '收藏',
-                    removeFavoriteSearchButtonTitle: '从收藏中移除'
+                    recentSearchesTitle: '検索履歴',
+                    noRecentSearchesText: '検索履歴がありません',
+                    saveRecentSearchButtonTitle: '検索履歴に保存',
+                    removeRecentSearchButtonTitle: '検索履歴から削除',
+                    favoriteSearchesTitle: 'コレクション',
+                    removeFavoriteSearchButtonTitle: 'コレクションから削除'
                 },
                 errorScreen: {
-                    titleText: '无法获取结果',
-                    helpText: '你可能需要检查你的网络连接'
+                    titleText: '結果を取得できませんでした',
+                    helpText: 'ネットワーク接続をチェックする必要があるかもしれません'
                 },
                 footer: {
-                    selectText: '选择',
-                    navigateText: '切换',
-                    closeText: '关闭',
-                    searchByText: '搜索提供者'
+                    selectText: 'せんたく',
+                    navigateText: '切り替え',
+                    closeText: '閉じる',
+                    searchByText: 'プロバイダの検索'
                 },
                 noResultsScreen: {
-                    noResultsText: '无法找到相关结果',
-                    suggestedQueryText: '你可以尝试查询',
-                    reportMissingResultsText: '你认为该查询应该有结果？',
-                    reportMissingResultsLinkText: '点击反馈'
+                    noResultsText: '関連する結果を見つけることができませんでした',
+                    suggestedQueryText: '検索を試みることができます',
+                    reportMissingResultsText: 'クエリに結果があると思いますか?',
+                    reportMissingResultsLinkText: 'クリックフィードバック'
                 }
             }
         }
