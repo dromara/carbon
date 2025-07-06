3072f046f118e7897b41bf0f77e9864293dd136c
kuafuRace <245629560@qq.com>
2025-07-06 23:48:03 +0800

    feat: Optimize language pack loading and processing logic

--- language.go
+++ language.go
@@ -43,23 +43,26 @@ func (lang *Language) SetLocale(locale string) *Language {
 	if lang == nil || lang.Error != nil {
 		return lang
 	}
-
 	if locale == "" {
 		lang.Error = ErrEmptyLocale()
 		return lang
 	}
 
+	fileName := fmt.Sprintf("%s/%s.json", lang.dir, locale)
+	bs, err := fs.ReadFile(fileName)
+	if err != nil {
+		lang.Error = fmt.Errorf("%w: %w", ErrNotExistLocale(fileName), err)
+		return lang
+	}
+
+	var resources map[string]string
+	_ = json.Unmarshal(bs, &resources)
+
 	lang.rw.Lock()
 	defer lang.rw.Unlock()
 
 	lang.locale = locale
-	fileName := fmt.Sprintf("%s/%s.json", lang.dir, locale)
-	bytes, err := fs.ReadFile(fileName)
-	if err != nil {
-		lang.Error = ErrNotExistLocale(fileName)
-		return lang
-	}
-	_ = json.Unmarshal(bytes, &lang.resources)
+	lang.resources = resources
 	return lang
 }
 
@@ -68,7 +71,6 @@ func (lang *Language) SetResources(resources map[string]string) *Language {
 	if lang == nil || lang.Error != nil {
 		return lang
 	}
-
 	if len(resources) == 0 {
 		lang.Error = ErrEmptyResources()
 		return lang
@@ -77,17 +79,12 @@ func (lang *Language) SetResources(resources map[string]string) *Language {
 	lang.rw.Lock()
 	defer lang.rw.Unlock()
 
-	if len(lang.resources) == 0 {
-		lang.resources = resources
-	}
-	for i := range resources {
-		if !slices.Contains(validResourcesKey, i) {
+	for k, v := range resources {
+		if !slices.Contains(validResourcesKey, k) {
 			lang.Error = ErrInvalidResourcesError(resources)
 			return lang
 		}
-		if _, ok := lang.resources[i]; ok {
-			lang.resources[i] = resources[i]
-		}
+		lang.resources[k] = v
 	}
 	return lang
 }
@@ -99,14 +96,15 @@ func (lang *Language) translate(unit string, value int64) string {
 	}
 
 	lang.rw.Lock()
-	defer lang.rw.Unlock()
-
 	if len(lang.resources) == 0 {
 		lang.rw.Unlock()
 		lang.SetLocale(DefaultLocale)
 		lang.rw.Lock()
 	}
-	slice := strings.Split(lang.resources[unit], "|")
+	resources := lang.resources
+	defer lang.rw.Unlock()
+
+	slice := strings.Split(resources[unit], "|")
 	number := getAbsValue(value)
 	if len(slice) == 1 {
 		return strings.Replace(slice[0], "%d", strconv.FormatInt(value, 10), 1)
