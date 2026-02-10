4da5485d05bb85142e3a04165564c62dc90bcee3
gouguoyin <245629560@qq.com>
2026-02-10 11:49:03 +0800

    remove top ad

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,9 @@
 
 import DefaultTheme from 'vitepress/theme'
 import './vars.css'
+import './custom.css'
 import AsideAd from './components/AsideAd.vue'
+import HomeBanner from './components/HomeBanner.vue'
 import { h } from 'vue'
 
 declare var _hmt: any;
@@ -19,7 +21,8 @@ export default {
     extends: DefaultTheme,
     Layout: () => {
         return h(DefaultTheme.Layout, null, {
-            'aside-outline-after': () => h(AsideAd)
+            'aside-outline-after': () => h(AsideAd),
+            'home-hero-info-after': () => h(HomeBanner)
         })
     }
 }
